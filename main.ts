acd9d62c65b81259386bdb22cd76d9f74b637fa8
EngNelson <bechem200@gmail.com>
2022-04-12 23:20:16 +0100

    types

--- main.ts
+++ main.ts
@@ -14,5 +14,12 @@
 
 // doSomething();
 
-let count = 5;
-count = "a";
+// let count = 5;
+// count = "a";
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, "a", false];
