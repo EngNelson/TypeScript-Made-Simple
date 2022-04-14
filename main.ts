72d9cd0730efe4b7876c77889bd8a36a18db7723
EngNelson <bechem200@gmail.com>
2022-04-14 23:23:34 +0100

    constructor

--- main.ts
+++ main.ts
@@ -54,6 +54,11 @@ class Point {
   x: number;
   y: number;
 
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
   draw() {
     console.log("X: " + this.x + ", Y: " + this.y);
   }
@@ -62,7 +67,5 @@ class Point {
   }
 }
 
-let point = new Point();
-point.x = 1;
-point.y = 2;
+let point = new Point(1, 2);
 point.draw();
